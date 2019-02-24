5e7c7d766487d469e7340d50a6f2c44104abf010
Zee <zeeshansmann@hotmail.com>
2019-02-24 10:17:51 -0500

    Commit from Bash command

--- scripts/process.sh
+++ scripts/process.sh
@@ -2,3 +2,5 @@
 echo "hello world"
 echo "Completed"
 echo "Adding new line to the process.sh script"
+echo "This line added from git bash command prompt"
+echo "This is also added from Bash command"
